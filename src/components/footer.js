f982ab988eef40940039220bce1e60a8c21ab7b4
SARAS KUMAR <72179931+saras527725@users.noreply.github.com>
2025-03-19 15:41:37 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -23,7 +23,7 @@ function AppFooter() {
 
   return (
     <Container fluid>
-      <div className="copyright">&copy; 2022 Corporate. All Right Reserved.</div>
+      <div className="copyright">&copy; 2025 Corporate. All Right Reserved.</div>
       <div className="socials">
         <ul>
           <li><a href="https://www.facebook.com"><i className="fab fa-facebook-f"></i></a></li>
